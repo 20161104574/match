45762f37db344f20504a6390ed6717e18b4c3a24
20161104574 <2460779735>
2018-06-27 08:59:28 +0800

    1
    1

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  match
+//
+//  Created by 20161104574 on 18/6/26.
+//  Copyright © 2018年 20161104574. All rights reserved.
+//
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <streambuf>
+using namespace std;
+
+int main()
+{
+    //定义文件输出流
+    ofstream oFile;
+    
+    //打开要输出的文件
+    oFile.open("scoresheet.csv", ios::out | ios::trunc);    // 这样就很容易的输出一个需要的excel 文件
+    oFile << "姓名" << "," << "年龄" << "," << "班级" << "," << "班主任" << endl;
+    oFile << "张三" << "," << "22" << "," << "1" << "," << "JIM" << endl;
+    oFile << "李四" << "," << "23" << "," << "3" << "," << "TOM" << endl;
+    
+    oFile.close();
+    
+    
+    //打开要输出的文件
+    ifstream iFile("scoresheet.csv");
+    string   readStr((std::istreambuf_iterator<char>(iFile)),  std::istreambuf_iterator<char>());
+    cout <<  readStr.c_str();
+    
+    return 0;
+}
